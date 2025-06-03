03a9d9f77e5f4048c62796eb966d5e05ed17bd10
Juan Caballero <168228657+JuanCaballero09@users.noreply.github.com>
2025-06-03 18:32:45 -0500

    vista editar (#85)

--- Ruby-Proyect/app/controllers/pages_controller.rb
+++ Ruby-Proyect/app/controllers/pages_controller.rb
@@ -1,28 +1,27 @@
 class PagesController < ApplicationController
-  
+  before_action :authenticate_user!
+
   def index
     @seccion = params[:seccion] || "home"
-    
+
     @grupos = Grupo.includes(:products)
   end
-  
+
   def home
     @seccion = "home"
     render :index
-    
   end
 
-  def dashboard
-  end
-
-  def menu
-    @seccion = "menu"
+  def edit
+    @seccion = "edit"
+    @resource = current_user
+    @resource_name = :user
     render :index
   end
+
 
   def carrito
     @seccion = "carrito"
     render :index
   end
-  
 end
